f072db5ebdcc459e96c2d14d5db96297f5700b65
atramui11 <nikhil_trehan@alumni.brown.edu>
2021-04-03 16:03:29 -0400

    homescreen and card latest

---
+++ app/components/HomeScreen.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Box from '@material-ui/core/Box';
+import {Card} from "./Card"
+
+export function HomeScreen() {
+    // const classes = useStyles();
+
+    return (
+        <>
+            <Card/>
+            <Card/>
+            <Card/>
+        </>
+
+    );
+}

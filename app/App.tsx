844e7a0d26a9029af3d4a101e7b7d1ee4249ca1e
atramui11 <nikhil_trehan@alumni.brown.edu>
2021-04-03 10:13:32 -0400

    video component

--- app/App.tsx
+++ app/App.tsx
@@ -2,11 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//material UI component imports
+import VideoComponent from "./components/VideoComponent"
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>aTEST open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <VideoComponent/>
     </View>
   );
 }
---
+++ app/components/VideoComponent.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+// import makeStyles from "@material-ui/core/styles/makeStyles";
+
+
+
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//         margin: theme.spacing(2),
+//     },
+//     title: {
+//         marginLeft: theme.spacing(3),
+//         fontSize: "medium",
+//     },
+//     blurb: {
+//         fontSize: "medium",
+//         marginLeft: theme.spacing(10),
+//         marginRight: theme.spacing(10),
+//         // marginTop: theme.spacing(2),
+//         marginBottom: theme.spacing(2),
+//     },
+//     row: {
+//         display: "flex",
+//         flexDirection: "row",
+//         alignItems: "center",
+//     },
+
+
+// }));
+
+
+
+function VideoComponent() {
+    // const classes = useStyles();
+    
+    return (
+        <div >
+          <iframe
+		      width="853"
+		      height="480"
+		      src={`https://www.youtube.com/embed/rokGy0huYEA`}
+		      frameBorder="0"
+		      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		      allowFullScreen
+		      title="Embedded youtube"
+		  />
+        </div>
+    );
+}
+
+export default VideoComponent;
